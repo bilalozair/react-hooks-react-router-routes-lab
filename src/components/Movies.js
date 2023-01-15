75597bea022beab081e82001fbe06c66e664e61b
bilalozair <bo569@nyu.edu>
2023-01-15 15:32:46 -0500

    commit tree 7113ffb25290d9cfb5d29df7f3873944141ba73c

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,31 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  console.log("movies data: ", movies)
+  const moviesList =  movies.map((movie) => {
+    
+      return (     
+          <div key={movie.title}>
+            <h1>{movie.title}</h1>
+            <p>{movie.time} minutes</p>
+            <ul>
+              {movie.genres.map((genre) => {
+                return <li key = {genre}>{genre}</li>
+              })}
+            </ul>
+          </div>
+      )
+    });
+
+  console.log("movies List:", moviesList)
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {moviesList}
+    </div>
+  )
+ 
+  
 }
 
 export default Movies;
